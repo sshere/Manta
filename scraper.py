c6323708db613ffb5fb823f68bd157d3721d8f56
Stephan Shere <sshere1985@gmail.com>
2015-08-29 16:33:39 -0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -22,3 +22,22 @@
 # All that matters is that your final data is written to an SQLite database
 # called "data.sqlite" in the current working directory which has at least a table
 # called "data".
+from splinter import Browser
+
+with Browser("phantomjs") as browser:
+    # Optional, but make sure large enough that responsive pages don't
+    # hide elements on you...
+    browser.driver.set_window_size(1280, 1024)
+
+    # Open the page you want...
+    browser.visit("https://morph.io")
+
+    # submit the search form...
+    browser.fill("q", "parliament")
+    button = browser.find_by_css("button[type='submit']")
+    button.click()
+
+    # Scrape the data you like...
+    links = browser.find_by_css(".search-results .list-group-item")
+    for link in links:
+        print link['href']
